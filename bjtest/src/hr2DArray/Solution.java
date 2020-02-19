28f6859ea163b698aded3ce484b13efbed52ddd8
dypark <lorh@naver.com>
2020-02-19 18:10:49 +0900

    해커랭크 문제 풀이중

---
+++ bjtest/src/hr2DArray/Solution.java
@@ -0,0 +1,56 @@
+package hr2DArray;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the hourglassSum function below.
+    static int hourglassSum(int[][] arr) {
+    	
+    	int highestSum = 0;
+    	
+    	for(int i = 0; i < arr.length-2; i++) {
+    		
+    		int curSum = 0;
+    		
+    		for(int j = 0; j < arr[0].length-2; j++) {
+    			
+    		}
+    	}
+    	
+    	return highestSum;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int[][] arr = new int[6][6];
+
+        for (int i = 0; i < 6; i++) {
+            String[] arrRowItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int j = 0; j < 6; j++) {
+                int arrItem = Integer.parseInt(arrRowItems[j]);
+                arr[i][j] = arrItem;
+            }
+        }
+
+        int result = hourglassSum(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
